bab974969809806ac7c287946670394b1dd7e24e
kbph05 <horvatk@shaw.ca>
2025-10-29 18:45:42 -0700

    able to read the encoder (I havent tested it yet)

--- light_sampler/hal/include/hal/encoder.h
+++ light_sampler/hal/include/hal/encoder.h
@@ -6,12 +6,14 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+
+#include <gpiod.h>
+#include <stdio.h>
+
+#define CHIPNAME "gpiochip0" // Typically the name of your GPIO chip
+
 // Use the rotary encoder to control how fast the LED blinks (using PWM).
 // This LED emitter is designed to flash directly at the “detector” light sensor.
 int read_encoder(void);
-
-// Set the speed of the LED emitter using the rotary encoder.
-void set_blink_speed(int speed);
-
 
 #endif
