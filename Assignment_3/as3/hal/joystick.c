c4e072e42dbbf36f2e290cae1d4b0ad56379098e
kbph <151966470+kbph05@users.noreply.github.com>
2025-11-20 11:10:58 -0800

    Create joystick.c

---
+++ Assignment_3/as3/hal/joystick.c
@@ -0,0 +1,22 @@
+#ifndef _JOYSTICK_H_
+#define _JOYSTICK_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+typedef enum {
+    IDLE,
+    JS_UP,
+    JS_DOWN,
+    JS_LEFT,
+    JS_RIGHT
+} Direction;
+
+Direction joystick();
+
+#endif
