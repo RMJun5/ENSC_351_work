758f45fbbe11334d85346a03c186c1738d7251f9
kbph <151966470+kbph05@users.noreply.github.com>
2025-11-20 13:24:22 -0800

    Added enum for rotations

--- Assignment_3/as3/hal/encoder.c
+++ Assignment_3/as3/hal/encoder.c
@@ -10,6 +10,11 @@
 #include <gpiod.h> // preferably want to try an SPI approach to this instead
 #include <stdio.h>
 
+typedef enum 
+{
+    CW,
+    CCW
+} Rotation;
 
 // Use the rotary encoder to control how fast the LED blinks (using PWM).
 // This LED emitter is designed to flash directly at the “detector” light sensor.
