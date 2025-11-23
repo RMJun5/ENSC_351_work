1cc2f36264997ab835c59887a516f768183f4e01
kbph05 <horvatk@shaw.ca>
2025-11-22 23:35:47 -0800

    updated build system

--- Assignment_3/build.sh
+++ Assignment_3/build.sh
@@ -1,4 +1,8 @@
+# Unset PKG_CONFIG for ALSA host
 rm -rf build
+export PKG_CONFIG_SYSROOT_DIR=""
+export PKG_CONFIG_PATH=""
+
 cmake -B build -S . \
   -DCMAKE_TOOLCHAIN_FILE=$(pwd)/aarch64-toolchain.cmake
 cmake --build build
